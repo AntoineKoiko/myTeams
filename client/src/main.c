e02b7756b2494e48b249b67d195c318541e01b7b
AntoineKoiko <antoine.couacault@epitech.eu>
2021-05-09 22:10:43 +0200

    feat(server, client: main.c): add -help option

--- client/src/main.c
+++ client/src/main.c
@@ -6,12 +6,26 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "client.h"
+
+const char *help_panel = "USAGE: ./myteams_cli ip port\n"
+"\tip\tis the server ip address on which the server socket listens\n"
+"\tport\tis the port number on which the server socket listens\n";
+
+static int my_help(int ac, char **av)
+{
+    if (ac == 2 && strcmp(av[1], "-help") == 0) {
+        printf("%s", help_panel);
+        return EXIT_SUC;
+    }
+    return EXIT_FAIL;
+}
 
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+    if (my_help(ac, av) == EXIT_SUC)
+        return EXIT_SUC;
     printf("hello world\n");
     return EXIT_SUC;
 }
--- server/src/main.c
+++ server/src/main.c
@@ -6,12 +6,28 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "server.h"
+#include "logging_server.h"
+
+const char *help_panel = "USAGE: ./myteams_server port\n"
+"\n"
+"\tport\tis the port number on which the server socket listens.\n";
+
+static int my_help(int ac, char **av)
+{
+    if (ac == 2 && strcmp(av[1], "-help") == 0) {
+        printf("%s", help_panel);
+        return EXIT_SUC;
+    }
+    return EXIT_FAIL;
+}
 
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+    if (my_help(ac, av) == EXIT_SUC)
+        return EXIT_SUC;
     printf("hello world\n");
+    server_event_team_created("team_uuid", "team_name", "user_uuid");
     return EXIT_SUC;
 }
