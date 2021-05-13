86ef66f208977bd64d87e44aa27ce4b80fa7c078
Aurelien Joncour <aurelien.joncour@epitech.eu>
2021-05-13 10:55:13 +0200

    feat(libs/) struct connection_t.h

---
+++ libs/myteams/connection_t.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** B-NWP-400-REN-4-1-myteams-aurelien.joncour
+** File description:
+** connection_t
+*/
+
+#ifndef CONNECTION_T_H_
+#define CONNECTION_T_H_
+
+#include <netinet/in.h>
+
+typedef struct {
+    in_port_t port;
+    int socket;
+    struct sockaddr_in addr;
+    char *hostname;
+    unsigned char input_buff[1024];
+    unsigned char output_buff[1024];
+} connection_t;
+
+#endif /* !CONNECTION_T_H_ */
