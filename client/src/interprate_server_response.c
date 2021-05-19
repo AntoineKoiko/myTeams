651239bb22a079845c88042cdbae422666230b33
Aurelien Joncour <aurelien.joncour@epitech.eu>
2021-05-19 13:57:33 +0200

    fix(interprate_serve_response.c) change bad incrementation of old_cursor

--- client/src/interprate_server_response.c
+++ client/src/interprate_server_response.c
@@ -34,7 +34,7 @@ static bool check_packet(teams_client_t *client, size_t *old_cursor)
     get_int(&code, client->client.input_buff+it, &it, false);
     find_code_function(code, packet_size, client->client.input_buff+it);
     it += packet_size - sizeof(int);
-    *old_cursor = *old_cursor + it;
+    *old_cursor = it;
     return true;
 }
 
