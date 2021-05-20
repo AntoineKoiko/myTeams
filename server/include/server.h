a49cffa792d8ec61012ac73548284e7f221016ba
AntoineKoiko <antoine.couacault@epitech.eu>
2021-05-20 14:37:11 +0200

    feat(src/request/prepare_buffer_uuid): prepare_buffer with a protocol and an uuid

--- server/include/server.h
+++ server/include/server.h
@@ -134,6 +134,8 @@ size_t prepare_reply_buffer(unsigned char *buff, reply_t *reply, int code,
                         size_t *cursor);
 size_t prepare_user_buffer(unsigned char *buff, user_t *user, int code,
                             size_t *cursor);
+size_t prepare_uuid_buffer(unsigned char *buff, uuid_t uuid, int code, 
+                        size_t *cursor);
 
 //requests
 int login_request(teams_server_t *server, session_list_t *session,
