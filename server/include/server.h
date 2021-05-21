a6cc035ec3b29444a39cc399564ab6c0aeea2a49
AntoineKoiko <antoine.couacault@epitech.eu>
2021-05-21 10:03:39 +0200

    feat(src/requests/send.c): implement event to the receiver with new function find_user_session

--- server/include/server.h
+++ server/include/server.h
@@ -190,6 +190,9 @@ int info_channel_request(teams_server_t *server, session_list_t *session,
 int info_thread_request(teams_server_t *server, session_list_t *session,
                         char **argv);
 
+int send_request(teams_server_t *server, session_list_t *session,
+                        char **argv);
+
 team_node_t *find_team_by_uuid(database_t *db, uuid_t team_uuid);
 team_node_t *find_team_by_name(database_t *db, const char *name);
 
--- server/src/requests/send.c
+++ server/src/requests/send.c
@@ -7,12 +7,28 @@
 
 #include "server.h"
 
-static void put_msg_in_db(teams_server_t *server, msg_t *msg,
-                            user_node_t *user)
-{
-    //msg_node_t *msg_node = new_msg_node();
+// static void put_msg_in_db(teams_server_t *server, msg_t *msg,
+//                             user_node_t *user)
+// {
+//     //msg_node_t *msg_node = new_msg_node();
 
-   // SLIST_INSERT(; user->conversation; next);
+//    // SLIST_INSERT(; user->conversation; next);
+// }
+
+static void msg_is_created(teams_server_t *server, N_U session_list_t *session,
+                            msg_t *msg)
+{
+    char rec_uuid[UUID_STR_LEN] = {0};
+    char send_uuid[UUID_STR_LEN] = {0};
+    session_list_t *dest_session = find_user_session_by_uuid(server,
+                                                        msg->receiver_uuid);
+    uuid_unparse_lower(msg->receiver_uuid, rec_uuid);
+    uuid_unparse_lower(msg->sender_uuid, send_uuid);
+    //put_msg_in_db(server, msg, session->user);
+    if (dest_session && dest_session->user->user_data->status == CONNECTED)
+        prepare_msg_buffer(dest_session->cnt.output_buff, msg, 246,
+                            &dest_session->cnt.output_size);
+    server_event_private_message_sent(send_uuid, rec_uuid, msg->msg_body);
 }
 
 int send_request(teams_server_t *server, session_list_t *session,
@@ -23,7 +39,7 @@ int send_request(teams_server_t *server, session_list_t *session,
     msg_t *msg = NULL;
 
     uuid_parse(argv[0], dest_uuid);
-    dest = find_user_by_uuid(server->database, argv[0]);
+    dest = find_user_by_uuid(server->database, dest_uuid);
     if (!dest) {
         prepare_uuid_buffer(session->cnt.output_buff, dest_uuid, 411,
                             &session->cnt.output_size);
@@ -32,8 +48,6 @@ int send_request(teams_server_t *server, session_list_t *session,
     msg = new_msg(session->user->user_data->user_uuid, dest_uuid, argv[1]);
     if (!msg)
         return EXIT_FAILURE;
-    put_msg_in_db(server, msg, session->user);
-    //send event to the receiver
-    server_event_private_message_sent(msg->sender_uuid, msg->receiver_uuid,
-                                        msg->msg_body);
+    msg_is_created(server, session, msg);
+    return EXIT_SUCCESS;
 }
