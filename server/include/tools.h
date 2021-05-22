a30f68bc0b5b69a3a21323756d4986b28e8ad8c8
Aurelien Joncour <aurelien.joncour@epitech.eu>
2021-05-22 12:11:38 +0200

    feat(serrver/src/tools/str_array.c) add str_array_len fct

--- server/include/tools.h
+++ server/include/tools.h
@@ -17,4 +17,6 @@ int read_and_check(int fd, void *buff, size_t read_size);
 NON_NULL(2)
 int write_and_check(int fd, const void *buff, size_t write_size);
 
+NON_NULL(1) size_t str_array_len(const char **str_array);
+
 #endif // SERVER_TOOLS_H
