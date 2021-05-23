ae3730c8c6f22301485beec74235cccdbf50de7d
arthur.jourdan <arthur.jourdan@epitech.eu>
2021-05-23 22:01:07 +0200

    fix(server/src/requests/create_thread.c): change deprecated function call

--- server/src/requests/create_thread.c
+++ server/src/requests/create_thread.c
@@ -7,16 +7,17 @@
 
 #include "server.h"
 
-static int thread_created(teams_server_t *server, session_list_t *session,
-                        thread_t *thread)
+static int thread_created(
+    teams_server_t *server, session_list_t *session, thread_t *thread)
 {
     session_list_t *s = NULL;
     size_t *cursor = &session->cnt.output_size;
 
     prepare_thread_buffer(session->cnt.output_buff, thread, 234, cursor);
-    STAILQ_FOREACH(s, &server->session_head, next) {
-        if (is_sub_and_connected(server->database, session->team_ctx,
-                                s->user->user_data)) {
+    STAILQ_FOREACH(s, &server->session_head, next)
+    {
+        if (is_sub_and_connected(
+                server->database, session->team_ctx, s->user->user_data)) {
             cursor = &s->cnt.output_size;
             prepare_thread_buffer(s->cnt.output_buff, thread, 244, cursor);
         }
@@ -35,47 +36,58 @@ static int thread_created(teams_server_t *server, session_list_t *session,
 //     return EXIT_SUCCESS;
 // }
 
-//TODO : push new obj in db
-static thread_t *create_process(teams_server_t *server, session_list_t *ses,
-                                    char **argv)
+// TODO : push new obj in db
+static thread_t *create_process(
+    teams_server_t *server, session_list_t *ses, char **argv)
 {
     thread_t *thread = NULL;
-    channel_node_t *node = find_channel_by_uuid(server->database, ses->team_ctx,
-                                            ses->channel_ctx);
+    channel_node_t *node = find_channel_by_team(
+        server->database, ses->team_ctx, ses->channel_ctx);
 
     if (!node)
         return NULL;
     thread = new_thread(node->channel_data->channel_uuid,
-        ses->user->user_data->user_uuid, argv[0], argv[1]);
+        ses->user->user_data->user_uuid,
+        argv[0],
+        argv[1]);
     if (!thread)
         return NULL;
     return thread;
 }
 
-int create_thread_request(teams_server_t *server, session_list_t *session,
-                        char **argv)
+int create_thread_request(
+    teams_server_t *server, session_list_t *session, char **argv)
 {
     char thread_uuid[UUID_STR_LEN] = {0};
     char chan_uuid[UUID_STR_LEN] = {0};
     char user_uuid[UUID_STR_LEN] = {0};
     thread_t *thread = NULL;
 
-    if (find_thread_by_name(server->database, session->team_ctx,\
-session->channel_ctx, argv[0])) {
-        put_protocol(session->cnt.output_buff, sizeof(int), 402, &session->cnt.output_size);
+    if (find_thread_by_name(server->database,
+            session->team_ctx,
+            session->channel_ctx,
+            argv[0])) {
+        put_protocol(session->cnt.output_buff,
+            sizeof(int),
+            402,
+            &session->cnt.output_size);
         return EXIT_SUCCESS;
     }
-    thread = create_process(server, session, argv);;
+    thread = create_process(server, session, argv);
+    ;
     if (!thread) {
-        //creation_failed(session);
+        // creation_failed(session);
         return EXIT_FAILURE;
     } else {
         thread_created(server, session, thread);
         uuid_unparse_lower(thread->thread_uuid, thread_uuid);
         uuid_unparse_lower(thread->channel_uuid, chan_uuid);
         uuid_unparse_lower(thread->user_uuid, user_uuid);
-        server_event_thread_created(chan_uuid, thread_uuid, user_uuid,
-                                    thread->thread_title, thread->thread_body);
+        server_event_thread_created(chan_uuid,
+            thread_uuid,
+            user_uuid,
+            thread->thread_title,
+            thread->thread_body);
     }
     return EXIT_SUCCESS;
 }
